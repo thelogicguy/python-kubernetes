48c557c8a92395e32ea43d45c77d49500e399102
thelogicguy <107140254+thelogicguy@users.noreply.github.com>
2026-03-08 14:15:46 +0100

    modified provider.tf

--- terraform-configs/provider.tf
+++ terraform-configs/provider.tf
@@ -1,15 +1,15 @@
 provider "kubernetes" {
   host                   = minikube_cluster.minikube_docker.host
-  client_certificate     = base64decode(minikube_cluster.minikube_docker.client_certificate)
-  client_key             = base64decode(minikube_cluster.minikube_docker.client_key)
-  cluster_ca_certificate = base64decode(minikube_cluster.minikube_docker.cluster_ca_certificate)
+  client_certificate     = minikube_cluster.minikube_docker.client_certificate
+  client_key             = minikube_cluster.minikube_docker.client_key
+  cluster_ca_certificate = minikube_cluster.minikube_docker.cluster_ca_certificate
 }
 
 provider "helm" {
   kubernetes = {                                                                    
     host                   = minikube_cluster.minikube_docker.host
-    client_certificate     = base64decode(minikube_cluster.minikube_docker.client_certificate)
-    client_key             = base64decode(minikube_cluster.minikube_docker.client_key)
-    cluster_ca_certificate = base64decode(minikube_cluster.minikube_docker.cluster_ca_certificate)
+    client_certificate     = minikube_cluster.minikube_docker.client_certificate
+    client_key             = minikube_cluster.minikube_docker.client_key
+    cluster_ca_certificate = minikube_cluster.minikube_docker.cluster_ca_certificate
   }
 }
